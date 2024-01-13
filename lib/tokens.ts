7510839f0cda188cebcdc6d3dff3a551fc93e9a1
DEF4LT-303 <ryanrafi72@gmail.com>
2024-01-13 21:20:32 +0600

    login creds display

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -52,7 +52,7 @@ export const generatePasswordResetToken = async (email: string) => {
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  const expires = new Date(new Date().getTime() + 900 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const existingToken = await db.twoFactorToken.findFirst({
     where: { email }
