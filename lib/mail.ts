28223a3401c82ecc68c01bb47ad238a5a6997ba4
DEF4LT-303 <ryanrafi72@gmail.com>
2024-01-19 01:47:56 +0600

    dialog added

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,12 +2,13 @@ import { Resend } from "resend";
 import { mailOptions, transporter } from "./nodemailer";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_API_URL;
 
 export const sendVerificationEmail = async (
   email: string,
   token: string
 ) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`
 
   // await resend.emails.send({
   //   from: 'onboarding@resend.dev',
@@ -74,7 +75,7 @@ export const sendPasswordResetEmail = async (
   email: string,
   token: string
 ) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+  const resetLink = `${domain}/auth/new-password?token=${token}`
 
   // await resend.emails.send({
   //   from: 'onboarding@resend.dev',
